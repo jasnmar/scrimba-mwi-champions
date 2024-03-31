942c7d3bf91a6e4edb209ed2ba853e787584fb78
jasnmar <jasnmar@gmail.com>
2024-03-31 16:00:39 -0400

    one more try today

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import { Context } from "@netlify/edge-functions";
 
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
