a7d18b0e3f2f4e45e80b55cb1b48c6b1b8face55
jasnmar <jasnmar@gmail.com>
2024-03-31 15:38:33 -0400

    Champions project complete

---
+++ index.js
@@ -0,0 +1,45 @@
+import { dbObj } from "./env.js"
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const dbApp = initializeApp(dbObj)
+const database = getDatabase(dbApp)
+const endorsementsInDb = ref(database, "endorsementList")
+
+const listEl = document.getElementById("endorsements-list")
+
+setupPage()
+function setupPage() {
+    const publishBtn = document.getElementById("publish-button")
+    publishBtn.addEventListener("click", publishEndorsement)
+}
+
+onValue(endorsementsInDb, function(snapshot) {
+    const endorsments = Object.entries(snapshot.val())
+    listEl.innerHTML = "";
+    for (let i = 0; i < endorsments.length; i++){
+        addEndorsementToList(endorsments[i],)
+    }
+
+})
+
+const endosementInputEl = document.getElementById("text-input")
+function publishEndorsement() {
+    const endosementValue = endosementInputEl.value
+    const retVal = push(endorsementsInDb, endosementValue)
+}
+
+function addEndorsementToList(endorsementObject) {
+    const newListItem = document.createElement("li")
+    const endorsementID = endorsementObject[0]
+  
+    newListItem.textContent = endorsementObject[1]
+    newListItem.addEventListener("click", function() {
+        let exactLocationOfEndorsementInDB = ref(database, `endorsementList/${endorsementID}`)
+
+        remove(exactLocationOfEndorsementInDB)
+  
+    })
+    listEl.appendChild(newListItem)
+
+}
